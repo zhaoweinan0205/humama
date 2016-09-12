14bd5aaa97018d120cbe9be0b6362d3d592d35da
Ash <zhaoweinan0205@163.com>
2016-09-13 01:10:46 +0800

    增加了商品类目mapper类

---
+++ humama-manage-mapper/src/main/java/com/humama/manage/mapper/item/ItemCatMapper.java
@@ -0,0 +1,13 @@
+package com.humama.manage.mapper.item;
+
+import com.github.abel533.mapper.Mapper;
+import com.humama.manage.pojo.item.ItemCat;
+
+/**
+ * @Description: .
+ * @Author: ZHaoWeiNan .
+ * @CreatedTime: 2016/9/12 .
+ * @Version: 1.0 .
+ */
+public interface ItemCatMapper extends Mapper<ItemCat> {
+}
